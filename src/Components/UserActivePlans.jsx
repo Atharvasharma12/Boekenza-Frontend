11ba2bfd75a80c4e58dd10bdc59fd7045f7034d3
Atharva sharma <89251910+Atharvasharma12@users.noreply.github.com>
2023-04-24 01:55:32 +0530

    Doc: minor changes

--- src/Components/UserActivePlans.jsx
+++ src/Components/UserActivePlans.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 import "./UserActivePlans.css";
